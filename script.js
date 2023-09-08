079297707aa5cbd73562aec25d4ec3543d3639c7
Kanishka-1102 <131690802+Kanishka-1102@users.noreply.github.com>
2023-09-09 01:28:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code can be added here for interactive features if needed
+document.getElementById("getStartedButton").addEventListener("click", function() {
+    // You can add functionality here, like navigating to another page or displaying a form.
+    // For this example, we will simply display an alert.
+    alert("Let's get started on your journey to better mental health!");
+});
